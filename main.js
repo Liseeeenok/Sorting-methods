ab171cf1ba692ee7ca7347187c57b111ac1b126f
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2022-09-24 21:29:04 +0800

    Added bubble sort

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-console.log('work!');
-
+//Элементы DOM дерева
 let div = document.getElementById('div');
 let select = document.getElementById('select');
 let buts = document.querySelectorAll('button')
-
+let timer = document.getElementById('timer')
+// Создание таблицы
 let table = document.createElement('table');
 table.id = "table";
-
+//Тип сортировки
+let typeSort = "1";
+//Инициализация наваний столбцов
 let newRow = table.insertRow(0);
 let newCell = newRow.insertCell(0);
-
-let typeSort = 1;
-
 newCell.innerHTML = 'Индекс'
 newCell = newRow.insertCell(1);
 newCell.innerHTML = 'Имя'
@@ -25,7 +24,7 @@ newCell = newRow.insertCell(5);
 newCell.innerHTML = 'Дата рождиения'
 newCell = newRow.insertCell(6);
 newCell.innerHTML = 'Номер'
-
+//Функция для заполнения таблицы
 function getElinTable(obj) {
     let newRow = table.insertRow(obj.Индекс);
     let newCell = newRow.insertCell(0);
@@ -43,37 +42,95 @@ function getElinTable(obj) {
     newCell = newRow.insertCell(6);
     newCell.innerHTML = obj.Номер
 }
-
+//Заполнение таблицы
 data.forEach(element => {
     getElinTable(element)
 });
-
+//Добавление таблицы в DOM
 div.appendChild(table);
-
+//Слушатель выбора сортировка
 select.addEventListener('change', function(event) {
     typeSort = select.value;
 })
-
+//Получение таблицы из DOM
 table = document.getElementById('table');
-console.log(buts)
-
-let test = table.querySelector('tbody')
-console.log(test)
-setTimeout(test.insertBefore(test.rows[3], test.rows[2]), 2000)
-test.insertBefore(test.rows[3], test.rows[2]);
-test.insertBefore(test.rows[10], test.rows[2]);
-test.insertBefore(test.rows[12], test.rows[2]);
-test.insertBefore(test.rows[12], test.rows[2]);
-/*
-while (true) {
-    if (table.moveRow) {        // Internet Explorer
-        setTimeout(table.moveRow (2, 3), 2000);
-    } 
-    else {        // Cross browser
-        let test = table.getElementsByTagName('tbody')
-        console.log(table.getElementsByTagName('tbody'))
-        test[0].id = "awdwada"
-        console.log(test[0].rows[2]);
-        setInterval(test[0].insertBefore(test[0].rows[3], test[0].rows[2]), 200000);
+//Слушатель кнопок
+buts.forEach(element => {
+    element.addEventListener('click', function(event) {
+        sortTable(event.target.id)
+    })
+})
+//Выбор по типу сортировки
+function sortTable(id) {
+    switch (typeSort) {
+        case ("1"):
+            sortingBubbleRow(id);
+            break;
+        case ("2"):
+            console.log('two sort');
+            break;
     }
-}*/
+}
+//Выбор по столбцу сортировки
+function sortingBubbleRow(id) {
+    switch (id) {
+        case ("but_1"):
+            sortingBubble(0);
+            break;
+        case ("but_2"):
+            sortingBubble(1);
+            break;
+        case ("but_3"):
+            sortingBubble(2);
+            break;
+        case ("but_4"):
+            sortingBubble(3);
+            break;
+        case ("but_5"):
+            sortingBubble(4);
+            break;
+        case ("but_6"):
+            sortingBubble(5);
+            break;
+        case ("but_7"):
+            sortingBubble(6);
+            break;
+    }
+}
+//Сортировка пузырьком
+function sortingBubble(index) {
+    let tbody = table.querySelector('tbody');
+    let sort = true;
+    let start = new Date();
+    if ((index == 0) || (index == 6)){
+        for (i=1;i<2000;i++) {
+            sort = true;
+            for(j=1;j<=2000-i;j++) {
+                if (parseInt(tbody.rows[j].cells[index].innerHTML) > parseInt(tbody.rows[j+1].cells[index].innerHTML)) {
+                    tbody.insertBefore(tbody.rows[j+1], tbody.rows[j])
+                    sort = false;
+                }
+            }
+            if (sort) {
+                let end = new Date();
+                console.log(end-start);
+                return;
+            }
+        }
+        return;
+    }
+    for (i=1;i<2000;i++) {
+        sort = true;
+        for(j=1;j<=2000-i;j++) {
+            if (tbody.rows[j].cells[index].innerHTML > tbody.rows[j+1].cells[index].innerHTML) {
+                tbody.insertBefore(tbody.rows[j+1], tbody.rows[j])
+                sort = false;
+            }
+        }
+        if (sort) {
+            let end = new Date();
+            console.log(end-start);
+            return;
+        }
+    }
+}
