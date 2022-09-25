042702a4f923003e653cc595450acd20a5ab2589
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2022-09-25 12:43:31 +0800

    Changed function redrawing table

--- main.js
+++ main.js
@@ -24,76 +24,84 @@ newCell = newRow.insertCell(5);
 newCell.innerHTML = 'Дата рождиения'
 newCell = newRow.insertCell(6);
 newCell.innerHTML = 'Номер'
-//Функция для заполнения таблицы
+//Функция для рисовки таблицы
 function getElinTable(obj) {
-    let newRow = table.insertRow(obj.Индекс);
-    let newCell = newRow.insertCell(0);
-    newCell.innerHTML = obj.Индекс
-    newCell = newRow.insertCell(1);
-    newCell.innerHTML = obj.Имя
-    newCell = newRow.insertCell(2);
-    newCell.innerHTML = obj.Фамилия
-    newCell = newRow.insertCell(3);
-    newCell.innerHTML = obj.Отчество
-    newCell = newRow.insertCell(4);
-    newCell.innerHTML = obj.Пол
-    newCell = newRow.insertCell(5);
-    newCell.innerHTML = obj.Дата
-    newCell = newRow.insertCell(6);
-    newCell.innerHTML = obj.Номер
+    for (i = 0; i < obj.length; i++) {
+        let newRow = table.insertRow(i+1);
+        let newCell = newRow.insertCell(0);
+        newCell.innerHTML = obj[i].Индекс
+        newCell = newRow.insertCell(1);
+        newCell.innerHTML = obj[i].Имя
+        newCell = newRow.insertCell(2);
+        newCell.innerHTML = obj[i].Фамилия
+        newCell = newRow.insertCell(3);
+        newCell.innerHTML = obj[i].Отчество
+        newCell = newRow.insertCell(4);
+        newCell.innerHTML = obj[i].Пол
+        newCell = newRow.insertCell(5);
+        newCell.innerHTML = obj[i].Дата
+        newCell = newRow.insertCell(6);
+        newCell.innerHTML = obj[i].Номер
+    }
 }
 //Заполнение таблицы
-data.forEach(element => {
-    getElinTable(element)
-});
+getElinTable(data);
 //Добавление таблицы в DOM
 div.appendChild(table);
+
+let buf;
+buf = data[3];
+data[3] = data[0];
+data[0] = buf;
+
 //Слушатель выбора сортировка
 select.addEventListener('change', function(event) {
+    getElinTable(data);
     typeSort = select.value;
 })
+/*
 //Получение таблицы из DOM
 table = document.getElementById('table');
 //Слушатель кнопок
 buts.forEach(element => {
     element.addEventListener('click', function(event) {
-        sortTable(event.target.id)
+        sortRow(event.target.id)
     })
 })
-//Выбор по типу сортировки
+//Выбор по типу сортировки пузырьком
 function sortTable(id) {
     switch (typeSort) {
         case ("1"):
-            sortingBubbleRow(id);
+            sortingBubble(id);
             break;
         case ("2"):
-            console.log('two sort');
+            sortingChoice(id);
             break;
     }
 }
 //Выбор по столбцу сортировки
-function sortingBubbleRow(id) {
+function sortRow(id) {
     switch (id) {
         case ("but_1"):
-            sortingBubble(0);
+            sortTable(0);
             break;
         case ("but_2"):
-            sortingBubble(1);
+            sortTable(1);
             break;
         case ("but_3"):
-            sortingBubble(2);
+            sortTable(2);
             break;
         case ("but_4"):
-            sortingBubble(3);
+            sortTable(3);
             break;
         case ("but_5"):
-            sortingBubble(4);
+            sortTable(4);
             break;
         case ("but_6"):
-            sortingBubble(5);
+            sortTable(5);
             break;
         case ("but_7"):
-            sortingBubble(6);
+            sortTable(6);
             break;
     }
 }
@@ -113,6 +121,7 @@ function sortingBubble(index) {
             }
             if (sort) {
                 let end = new Date();
+                timer.innerHTML = "Время: " + (end-start);
                 console.log(end-start);
                 return;
             }
@@ -129,8 +138,64 @@ function sortingBubble(index) {
         }
         if (sort) {
             let end = new Date();
+            timer.innerHTML = "Время: " + (end-start);
             console.log(end-start);
             return;
         }
     }
 }
+//Сортировка выбором
+function sortingChoice(index) {
+    let tbody = table.querySelector('tbody');
+    let sort = true;
+    let start = new Date();
+    let min;
+    if ((index == 0) || (index == 6)){
+        for (let i=1;i<2000;i++) {
+            min=i;
+            sort = true;
+            for(let j=i;j<2000;j++) {
+                if (parseInt(tbody.rows[j].cells[index].innerHTML) > parseInt(tbody.rows[j+1].cells[index].innerHTML)) {
+                    min = j+1;
+                    sort = false;
+                }
+            }
+            console.log(tbody.rows[min].cells[index].innerHTML)
+            tbody.insertBefore(tbody.rows[min], tbody.rows[i]);
+            if (sort) {
+                let end = new Date();
+                timer.innerHTML = "Время: " + (end-start);
+                console.log(end-start);
+                return;
+            }
+        }
+        return;
+    }
+    let buf;
+    console.log(data);
+    for (let i=0;i<2000;i++) {
+        min=i;
+        sort = true;
+        for(let j=i;j<2000;j++) {
+            if (data[min].Имя > data[j].Имя) {
+                min = j;
+                sort = false;
+            }
+        }
+        console.log(min);
+        buf = data[min];
+        data[min] = data[i];
+        data[i] = buf;
+        if (sort) {
+            let end = new Date();
+            timer.innerHTML = "Время: " + (end-start);
+            console.log(end-start);
+            console.log(data);
+            data.forEach(element => {
+                getElinTable(element)
+            });
+            return;
+        }
+    }
+}
+*/
